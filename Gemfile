39e0e512695f3b5320c6061acbd857f757b64275
Gregoire Schiller <gregoire.schiller@gmail.com>
2016-11-28 17:16:43 +0100

    clearing

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem "bootstrap-sass"
 gem "font-awesome-sass"
 gem "simple_form"
 gem "autoprefixer-rails"
+gem 'devise'
 
 ruby '2.3.1'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'pages#home'
   get 'cgv', to: 'pages#cgv'
   get 'mentionslegales', to: 'pages#mentionslegales'
+  resources :users, only: [:show]
 end
